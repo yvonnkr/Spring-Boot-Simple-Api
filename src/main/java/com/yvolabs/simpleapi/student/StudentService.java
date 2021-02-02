1300da23357942be6400e643f8c82f9b426d2e3a
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-02-02 00:30:26 +0000

    Student Repository and Inject it to StudentService

--- src/main/java/com/yvolabs/simpleapi/student/StudentService.java
+++ src/main/java/com/yvolabs/simpleapi/student/StudentService.java
@@ -1,25 +1,32 @@
 package com.yvolabs.simpleapi.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @Service // Same as @Component only that the @Service is for semantics
 public class StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
 
     public List<Student> getStudents() {
-        LocalDate dob = LocalDate.of(1999, Month.SEPTEMBER, 27);
-        LocalDate now = LocalDate.now();
-        int age = now.getYear() - dob.getYear();
 
-        return List.of(
-                new Student(1L,
-                        "john",
-                        "john@test.com",
-                        dob,
-                        age)
-        );
+        return studentRepository.findAll();
     }
 }
+
+/**
+ * LocalDate dob = LocalDate.of(1999, Month.SEPTEMBER, 27);
+ *         LocalDate now = LocalDate.now();
+ *         int age = now.getYear() - dob.getYear();
+ * new Student(1L,
+ *                         "john",
+ *                         "john@test.com",
+ *                         dob,
+ *                         age)
+ */
